09befc4606708ac007b5e158a47168d9b2714574
yangang <yang@ktwelve.com.cn>
2020-10-28 14:59:20 +0800

    fix: gulp打包

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp')
+const less = require('gulp-less')
+const autoprefixer = require('gulp-autoprefixer')
+const cleancss = require('gulp-clean-css')
+const postcss = require('gulp-postcss')
+
+gulp.task("less", function () {
+  return gulp.src("styles/*.less")
+    .pipe(less())
+    .pipe(postcss([require('autoprefixer')]))
+    .pipe(cleancss({
+      keepBreaks: true,
+      keepSpecialComments: '*'
+    }))
+    .pipe(gulp.dest("dist"))
+})
